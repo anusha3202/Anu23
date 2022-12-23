f3d5c3e7a906d9ab15c7d23510eaa07e7022f21f
anusha3202 <anushavigrahala3202@gmail.com>
2022-12-23 15:59:24 +0530

    first command

---
+++ src/main/java/Anusha11/Anusha11/App.java
@@ -0,0 +1,13 @@
+package Anusha11.Anusha11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
